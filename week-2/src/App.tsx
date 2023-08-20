df911fcb64d542ca5559aa374806dcae5d792d33
Raymond Lam <raymond.m.lam@gmail.com>
2023-08-20 13:02:24 +0200

    exercise-1

--- week-2/src/App.tsx
+++ week-2/src/App.tsx
@@ -1,6 +1,11 @@
+import { myAllan, myBarbie, myKen } from './exercise-1/doll'
 import './styles/App.css'
 
 function App() {
+	console.log('Barbie', myBarbie)
+	console.log('Ken', myKen.speak('Sublime!'))
+	console.log('Allan', myAllan.beAllan())
+
 	return <div className='wrapper'></div>
 }
 
