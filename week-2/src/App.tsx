7d3ed6efc86cdedcc33cf3a809e4c638edb94343
Raymond Lam <raymond.m.lam@gmail.com>
2023-08-20 11:34:07 +0200

    every day types

--- week-2/src/App.tsx
+++ week-2/src/App.tsx
@@ -1,26 +1,7 @@
 import './styles/App.css'
 
 function App() {
-	return (
-		<div className='wrapper'>
-			<h1 className='headline'>Create new post</h1>
-
-			<form className='form'>
-				<div className='text-inputs'>
-					<input type='text' placeholder='Post title' required />
-
-					<textarea placeholder='Write your post...' />
-				</div>
-
-				<button type='submit' className='btn'>
-					Post
-				</button>
-			</form>
-
-			<h2>Posts</h2>
-			<div className='post-wrapper'></div>
-		</div>
-	)
+	return <div className='wrapper'></div>
 }
 
 export default App
