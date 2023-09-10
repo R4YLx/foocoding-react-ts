0c673ca16f1efb1e79b45c463bd66d357b351dbe
Raymond Lam <raymond.m.lam@gmail.com>
2023-09-10 12:18:03 +0200

    changing state on button

--- week-1/src/App.tsx
+++ week-1/src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+	const [isPlaying, setIsPlaying] = useState(false)
+
+	function handleClick() {
+		setIsPlaying(!isPlaying)
+	}
+
 	return (
 		<div className='wrapper'>
 			<h1>Hello 👋</h1>
 			<h2>I'm Raymond</h2>
+
+			<button onClick={handleClick} className={isPlaying ? 'pause' : 'play'}>
+				{isPlaying ? 'Pause' : 'Play'}
+			</button>
 		</div>
 	)
 }
